29f82bba9983f16541fa1100b48337c08c47a3cd
Anish Patel <anishpatel@zipcoders-MacBook-Pro-9.local>
2019-05-23 13:59:32 -0400

    Completed

--- Main.java
+++ Main.java
@@ -1,22 +1,35 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-        int A = 0; //declare the variable guess
-        int B = 0; //declare the count
-        int C = 21; //number of time we guessed
+        int guess = 0; //declare the variable guess
+        int count = 0; //declare the count
+        int mysteryNumber = 21; //number of time we guessed
+        int previousNumber = 0;
         
         Scanner in = new Scanner(System.in); //declare scanner
         do {
             System.out.println("Guess a number between 1 and 50.");
             System.out.println("What is you number?");
-            A = in.nextInt();
-            B++;
+            guess = in.nextInt();
             
-            if (A > C) System.out.println("Too large");
-            if (A < C) System.out.println("Too small");
+            
+            if (guess > mysteryNumber) 
+            { System.out.println("Too large");
+            }
+            else if (guess < mysteryNumber)
+            {System.out.println("Too small");
+            }  
+            if (guess != previousNumber) 
+            {
+            count++;
+            previousNumber = guess; 
+            }
+           
         }
-        while (A != C);
+        while (guess != mysteryNumber);
+        
         System.out.println("Correct guess");
-        System.out.println("It took " + B + " tries to guess the correct number.");
-    }
-}
+        System.out.println("It took " + count + " tries to guess the correct number.");
+    
+        }
+    }   
